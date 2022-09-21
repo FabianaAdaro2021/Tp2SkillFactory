49745c1394218c741eb3d9e7f602556b9b5358ae
FabianaAdaro2021 <88856048+FabianaAdaro2021@users.noreply.github.com>
2022-09-21 20:52:57 -0300

    comienzo del proyecto

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = require('./router')
+//require("dotenv").config()
+const app = express();
+const port = 3000;
+//const Sequelize = require("sequelize");
+//const { errorLogger, errorParser } = require('./middlewares/errorHandler')
+
+//console.log(process.env.DATABASE_NAME)
+//app.use('/', router);
+//app.use([errorLogger, errorParser])
+
+  const requestTime = function(req, res, next) {
+   req.requestTime = Date.now();
+   next();
+  }
+   app.use(requestTime);
+
+   //app.use('/',function(req, res){
+   // var responseText = "Hello World !!!"
+   // responseText += 'Requested at:' + req.requestTime + '';
+   // res.send(responseText);
+  //});
+
+ //app.get('/Carlos',(req,res)=>{
+ // res.send('Hola Mundo!')
+ //});
+
+ app.use('/', router);
+
+app.listen(port, ()=>{
+  console.log(`Example app listening on port ${port}`)
+});
+  
