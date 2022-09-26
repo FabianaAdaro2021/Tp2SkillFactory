0016061235ba21f1fde62db1e2d680ab552ca4e0
FabianaAdaro2021 <88856048+FabianaAdaro2021@users.noreply.github.com>
2022-09-25 21:44:18 -0300

    correcciones

--- Controllers/car.js
+++ Controllers/car.js
@@ -1,25 +1,26 @@
 const db = require('../models/index');
 const { cars, user } = db;
 
+//todos los autos
 const getCars = (req, res, next) => {
     cars.findAll({ include: user})
         .then(cars => res.status(200).send(cars))
         .catch(err => next(err))
 }
-
+//autos po id
 const getCar = (req, res, next) => {
     const id = req.params.id;
     cars.findOne({ where: { id }})
         .then(car => res.status(200).send(car))
         .catch(err => next(err));
 }
-
+//agrega un auto
 const addCar = (req, res, next) => {
         cars.create(req.body)
         .then(car => res.status(201).send("car Created"))
         .catch(err => next(err))     
 }
-
+//edita un auto
 const editCar = (req, res, next) => {
     const id = req.params.id;
     const newcar = req.body;
@@ -28,7 +29,7 @@ const editCar = (req, res, next) => {
         .then(car => res.status(200).send("car Updated"))
         .catch(err => next(err));
 }
-
+//Elimina auto
 const deleteCar = (req, res, next) => {
     const id = req.params.id;
     cars.destroy({ where: { id } })
--- Middlewares/checks.js
+++ Middlewares/checks.js
@@ -5,7 +5,7 @@ const checkMail = (req, res, next) => {
     if(validateEmail(req.body.email)) {
         next();
     } else {
-        const error = new Error("Bad request");
+        const error = new Error("correo incorrecto");
         error.status = 400;
         next(error);
     }
